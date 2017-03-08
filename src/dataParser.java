d647e173c506e1eabc021c62c1d68e0d317f057c
ODrageset <olve.94@gmail.com>
2017-03-08 16:23:51 +0100

    fixed the path problem

--- src/dataParser.java
+++ src/dataParser.java
@@ -13,30 +13,40 @@ import org.apache.jena.vocabulary.RDF;
 
 public class dataParser {
 	public static void main(String[] args) throws FileNotFoundException, IOException {
-		String fileName = "data.csv";
+		String filePath = "data.csv";
+		
+		String curDir = System.getProperty("user.dir");
+		System.out.println("Current sys dir: " + curDir);
 		
 		Model model = ModelFactory.createDefaultModel();
 		model.setNsPrefix("", "http://www.PTWproject.org/ontology#");
 		
-		addFile(fileName, model);
+		addFile("data.csv", model);
 		
 	 	model.write(System.out, "TURTLE");
 	}
 	
 	private static void addFile (String fileName, Model model) throws FileNotFoundException, IOException {
 		File csvFile = new File(fileName);
-		System.out.println(csvFile.getAbsolutePath());
-		BufferedReader reader = new BufferedReader(new FileReader(csvFile));
-		String[] headers = reader.readLine().split(";");
-		Property[] properties = new Property[headers.length];
-		for (int i=0; i<headers.length; i++)
-			properties[i] = model.createProperty(model.getNsPrefixURI("") + headers[i].toLowerCase());
-		
-		String line = null;
-		while ((line = reader.readLine()) != null) {
-			addData(line, model, properties);
+		System.out.println("data filepath: " + csvFile.getAbsolutePath());
+		BufferedReader reader;
+		try {
+			reader = new BufferedReader(new FileReader(csvFile));
+			String[] headers = reader.readLine().split(";");
+			Property[] properties = new Property[headers.length];
+			for (int i=0; i<headers.length; i++)
+				properties[i] = model.createProperty(model.getNsPrefixURI("") + headers[i].toLowerCase());
+			
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				addData(line, model, properties);
+			}
+		 	reader.close();
+		 	
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-	 	reader.close();
+		
 	}
 	
 	private static void addData(String line, Model model, Property[] properties) {
--- src/filePathCheck.java
+++ src/filePathCheck.java
@@ -2,8 +2,10 @@ import java.io.File;
 
 public class filePathCheck {
 	public static void main(String[] args) {
-		File file = new File("data.csv");
+		File file = new File(".");
+		String curDir = System.getProperty("user.dir");
 		
 		System.out.println(file.getAbsolutePath());
+		System.out.println(curDir);
 	}
 }
