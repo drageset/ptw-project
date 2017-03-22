b0543197d088c8c723ea64d44fe3dc7ea7973280
vwa001 <vetlepw@hotmail.com>
2017-03-22 18:25:18 +0100

    Gjort grunnarbeidet for weatherParser, mangler å tolke og lese inn selve målingene

---
+++ src/WeatherDataParser.java
@@ -0,0 +1,290 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+import org.apache.jena.datatypes.xsd.XSDDateTime;
+import org.apache.jena.ontology.Individual;
+import org.apache.jena.ontology.OntModel;
+import org.apache.jena.rdf.model.Literal;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.vocabulary.RDF;
+import org.apache.jena.vocabulary.RDFS;
+import org.apache.jena.vocabulary.XSD;
+
+public class WeatherDataParser {
+	public final static String prefix = "ptw";
+	public final static String ns = "http://www.PTWproject.org/ontology#";
+	public final static String xsd = "http://www.w3.org/2001/XMLSchema#";
+
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		String filePath = "wdata.csv";
+		String outputFilePath = "semanticWData.ttl";
+		String notation = "TURTLE";
+
+		OntModel model = ModelFactory.createOntologyModel(); 
+
+		model.setNsPrefix(prefix, ns);
+
+		addFile(filePath, model);
+
+		writeModelToFile(model, outputFilePath, notation);
+
+		model.write(System.out, "TURTLE");
+	}
+
+	public static void parse(String filePath, String outputFilePath){
+		String notation = "TURTLE";
+
+		OntModel model = ModelFactory.createOntologyModel(); 
+
+		model.setNsPrefix(prefix, ns);
+
+		try {
+			addFile(filePath, model);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		writeModelToFile(model, outputFilePath, notation);
+	}
+
+	private static void addFile (String filePath, OntModel model) throws FileNotFoundException, IOException {
+		File csvFile = new File(filePath);
+		BufferedReader reader;
+		try {
+			reader = new BufferedReader(new FileReader(csvFile));
+			
+			//Tre linjer med drit i toppen
+			reader.readLine();
+			reader.readLine();
+			reader.readLine();
+			
+			String[] stHeaders = reader.readLine().split(";");
+			
+			Resource sensorClass = model.createResource( ns + "Sensor" );
+			Resource weatherSensorClass = model.createResource( ns + "WeatherSensor" );
+			weatherSensorClass.addProperty(RDFS.subClassOf, sensorClass);
+
+			Property inServiceSince = model.createProperty(ns + stHeaders[2]);
+			Property inServiceUntil = model.createProperty(ns + stHeaders[3]);
+			inServiceSince.addProperty(RDFS.domain, weatherSensorClass);
+			inServiceUntil.addProperty(RDFS.domain, weatherSensorClass);
+			inServiceSince.addProperty(RDFS.range, XSD.gYearMonth);
+			// Alle feltene for denne verdien er blanke, så lar range være uspesifisert enn så lenge
+//			inServiceUntil.addProperty(RDFS.range, XSD.gYearMonth);
+
+			Resource measurementClass = model.createResource(ns + "Measurement");
+			Resource weatherMeasurementClass = model.createResource(ns + "WeatherMeasurement");
+			weatherMeasurementClass.addProperty(RDFS.subClassOf, measurementClass);
+
+			Property dateTime = model.createProperty(ns + "dateTime");
+			dateTime.addProperty(RDFS.range, XSD.dateTime);
+
+			Property masl = model.createProperty(ns + stHeaders[4]);
+			masl.addProperty(RDFS.domain, weatherSensorClass);
+			masl.addProperty(RDFS.range, XSD.nonNegativeInteger);
+
+			Property inPrincipality = model.createProperty(ns + stHeaders[5]);
+			Property inCounty = model.createProperty(ns + stHeaders[6]);
+			Property inRegion = model.createProperty(ns + stHeaders[7]);
+
+			// Blokk med data om værstasjonene
+			for (int i = 0; i < 6; i++) {
+				String[] stvalues = reader.readLine().split(";");
+				
+				//Slår sammen stasjonnens navn og nummmer for å lage URI
+				//Sikkert lurt å også legge til nummer og eller navn hver for seg som properties
+				String stNum = stvalues[0];
+				String stName = stvalues[1];
+				String stIndividual = (stNum + "_" + stName).replaceAll(" ", "").replaceAll("-", "_");
+				Individual wSensor = model.createIndividual(ns + stIndividual, weatherSensorClass);
+				
+				//I drift fra
+				String stMonth = stvalues[2].substring(0, 3);
+				String stYear = stvalues[2].substring(4);
+				String stYearMonth = fixDateFormat(stMonth, stYear);
+				Literal yearMonth = model.createTypedLiteral(stYearMonth, xsd + "gYearMonth");
+				wSensor.addProperty(inServiceSince, yearMonth);
+				
+				//I drift til (evt. skippe dette feltet, da ingen av stasjonene har noe data der)
+				wSensor.addProperty(inServiceUntil, stvalues[3]);
+				
+				//Høyde over havet
+				wSensor.addLiteral(masl, Integer.parseInt(stvalues[4]));
+				
+				//Lokasjon
+				wSensor.addProperty(inPrincipality, stvalues[5]);
+				wSensor.addProperty(inCounty, stvalues[6]);
+				wSensor.addProperty(inRegion, stvalues[7]);	
+			}
+
+			//38 linjer med drit
+			for (int j = 1; j < 38; j++) {
+				reader.readLine();
+			}
+
+
+//			String[] headers = reader.readLine().split(";");
+//			Property[] properties = new Property[headers.length];
+//			for (int i=0; i<headers.length; i++){
+//				properties[i] = model.createProperty(model.getNsPrefixURI(prefix) + headers[i].toLowerCase());
+//				if(i > 2){
+//					properties[i].addProperty(RDFS.range, XSD.nonNegativeInteger);
+//				}
+//			}
+
+//			String line = null;
+//			while ((line = reader.readLine()) != null) {
+//				addData(line, model, properties, weatherMeasurementClass, sensor);
+//			}
+			reader.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	} //end addFile
+
+	
+	//Kopi fra trafficParser, ikke endret noe på enda
+	private static void addData(String line, Model model, Property[] properties, Resource measurementType, Resource sensor) {
+		Resource data = model.createResource();
+		data.addProperty(RDF.type, measurementType);
+		data.addProperty(model.getProperty(ns + "measuredBySensor"), sensor);
+
+		String[] values = line.split(";");
+		if(values.length == 9){
+			String xsdDateString = dmyToXSDate(values[0]);
+			String xsdTimeString = values[1] + ":00";
+			String xsdDateTimeString = xsdDateString + "T" + xsdTimeString;
+			Literal xsdDate = model.createTypedLiteral(xsdDateString, xsd + "date");
+			Literal xsdTime = model.createTypedLiteral(xsdTimeString, xsd + "time");
+			Literal xsdDateTime = model.createTypedLiteral(xsdDateTimeString, xsd + "dateTime");
+			data.addProperty(properties[0], xsdDate);
+			data.addProperty(properties[1], xsdTime);
+			data.addProperty(model.getProperty(ns + "dateTime"), xsdDateTime);
+			data.addProperty(model.getProperty(ns + "roadLaneMeasured"), model.getResource(sensor.getURI() + "_" + values[2]));
+			for (int i = 3; i < values.length; i++) {
+				Literal value = model.createTypedLiteral(values[i], xsd + "nonNegativeInteger");
+				data.addLiteral(properties[i], value);
+				//data.addLiteral(properties[i], Integer.parseInt(values[i]));
+			}	
+		}
+	} //end addData
+	
+	
+	/**
+	 * Skitten liten metode for å gjøre om mnd.år til XSD.gYearMonth
+	 * @param month
+	 * @param year
+	 * @return String med format som kan brukes til XSD.gYearMonth
+	 */
+	private static String fixDateFormat(String month, String year) {
+		String yyyymm = "";
+		
+		if(Integer.parseInt(year)>17) {
+			yyyymm += ("19" + year + "-");
+		} else yyyymm += ("20" + year + "-");
+		
+		switch (month) {
+		case "jan": yyyymm += "01";	break;
+		case "feb": yyyymm += "02";	break;
+		case "mar": yyyymm += "03";	break;
+		case "apr": yyyymm += "04";	break;
+		case "mai": yyyymm += "05";	break;
+		case "jun": yyyymm += "06";	break;
+		case "jul": yyyymm += "07";	break;
+		case "aug": yyyymm += "08";	break;
+		case "sep": yyyymm += "09";	break;
+		case "okt": yyyymm += "10";	break;
+		case "nov": yyyymm += "11";	break;
+		case "des": yyyymm += "12";	break;
+		}
+		return yyyymm;
+	}
+
+	/**
+	 * Gjør dato på formatet dd.mm.yyyy (dmy) om til xsd:date format, som er yyyy-mm-dd
+	 * @param dmy string
+	 * @return xsd:date format string
+	 */
+	private static String dmyToXSDate(String dmy){
+		try {
+			return calToXSDate(dmyToCal(dmy));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return "ErrorFailedToParse";
+	}
+
+	/**
+	 * Gjør dato på formatet xsd:datetime om til et objekt av typen XSDDateTime
+	 * @param xsd:dateTime string
+	 * @return XSDDateTime objekt
+	 */
+	@SuppressWarnings("unused")
+	private static XSDDateTime toXSDateTime(String xsdString){
+		Calendar cal;
+		try {
+			cal = Calendar.getInstance();
+			SimpleDateFormat xsdFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.ENGLISH);
+			cal.setTime(xsdFormat.parse(xsdString));
+			return new XSDDateTime(cal);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	/**
+	 * Gjør dato på formatet dd.mm.yyyy (dmy) om til Calendar
+	 * @param dmy string
+	 * @return Date
+	 * @throws ParseException 
+	 */
+	private static Calendar dmyToCal(String dmy) throws ParseException{
+		Calendar cal = Calendar.getInstance();
+		SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy", Locale.ENGLISH);
+		cal.setTime(sdf.parse(dmy));
+		return cal;  
+	}
+
+	/**
+	 * Gjør en calendar om til en streng med formatet til xsd:date
+	 * @param cal
+	 * @return xsd:date format string
+	 */
+	private static String calToXSDate(Calendar cal) {
+		Date time = cal.getTime();
+		SimpleDateFormat xsdFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
+		String xsdDate = xsdFormat.format(time);
+		return xsdDate;
+	}
+
+	/**
+	 * Writes a model to a file in a chosen notation
+	 * @param model The RDF model that you wish to write to a file
+	 * @param filename The filename of the file that you wish to write the RDF model to
+	 * @param notation The notation (i.e. TURTLE, JSON-LD) that you wish the file to be written in
+	 */
+	private static void writeModelToFile(Model model, String filename, String notation){
+		try{
+			PrintWriter writer = new PrintWriter(filename, "UTF-8");
+			model.write(writer, notation);
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
